8390816950456f04b587c9956a3bd0f5903dadce
AmigurumiShaders <mauriemma12@gmail.com>
2025-10-18 11:43:29 -0700

    Packaged new plugin version

---
+++ _Package/Paralogue/Intermediate/Build/Win64/UnrealEditor/Inc/ParalogueEditor/UHT/ParalogueEditor.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeParalogueEditor_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_ParalogueEditor;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_ParalogueEditor()
+	{
+		if (!Z_Registration_Info_UPackage__Script_ParalogueEditor.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/ParalogueEditor",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000040,
+				0x5A12A57B,
+				0x6337AC32,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ParalogueEditor.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_ParalogueEditor.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ParalogueEditor(Z_Construct_UPackage__Script_ParalogueEditor, TEXT("/Script/ParalogueEditor"), Z_Registration_Info_UPackage__Script_ParalogueEditor, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x5A12A57B, 0x6337AC32));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ _Package/Paralogue/Source/ParalogueRuntime/Public/PlogRtEncounterSegmentNodeData.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "CoreMinimal.h"
+//#include "ParalogueEncounter.h"
+
+#include "PlogRtEncounterSegmentNodeData.generated.h"
+
+/// <summary>
+/// Base class for storing the data that the user enters into each node
+/// </summary>
+UCLASS()
+class PARALOGUERUNTIME_API UPlogRtNodeUserData : public UObject {
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = "Meta")
+	FText Title;
+	/*Detailed description of this node, such as for the narrative situation it is intended for. Like a code comment*/
+	UPROPERTY(EditAnywhere, Category = "Meta")
+	FText Description; //maybe call this writer comment or something later but not worth overthinking now
+	//todo: it appears unreal maybe has some sort of "info" type, which might be a good thing to change this to
+
+	UPROPERTY(EditAnywhere, Category = "Meta")
+	bool SetAsStartNode = false;
+};
+
+/// <summary>
+/// Data for the main encounter segments
+/// </summary>
+UCLASS()
+class PARALOGUERUNTIME_API UPlogRtEncounterSegmentNodeUserData : public UPlogRtNodeUserData {
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = "Dialogue Content")//meta=(MultiLine=true) add this?
+		TArray<int> CharacterFaces;
+	
+	/*Linear segment of dialogue from the character, with each array element being one page of text*/
+	UPROPERTY(EditAnywhere,meta = (MultiLine = true, EditCondition = "!SetAsStartNode"), Category = "Dialogue Content")//meta=(MultiLine=true) add this?
+		TArray<FText> CharacterLines;
+	UPROPERTY(EditAnywhere, Category = "Dialogue Content") //meta=(MultiLine=true) add this?
+		TArray<FText> PlayerResponseOptions;
+
+	
+	UPROPERTY(EditAnywhere, Category = "Route Flags", meta = (FlagToSet = ""))
+	FName FlagToSet;
+	UPROPERTY(EditAnywhere, Category = "Route Flags")
+	bool FlagValue;
+};
+
+/// <summary>
+/// Simply the name of the route flag to check to decide on the branch
+/// </summary>
+UCLASS()
+class PARALOGUERUNTIME_API UPlogRtEncounterBranchNodeUserData : public UPlogRtNodeUserData {
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = "Route Flags", meta = (FlagToCheck = ""))
+	FName FlagToCheck;
+};
